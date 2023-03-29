7a362246691017c773836576bdc3bd69c322fb63
evgeniyakhatulkova <hatulkova.ev@gmail.com>
2023-03-29 19:50:13 +0200

    finilize, the method works

--- Bubble-Sort.rb
+++ Bubble-Sort.rb
@@ -1,10 +1,15 @@
 def bubble_sort(array)
-  array = array.each_with_index do |element, index|
-    temp = element
-    temp2 = array[index + 1]
+  itterations = array.length - 1
+
+  itterations.times do
+    array = array.each_with_index do |element, index|
+      temp = element
+      temp2 = array[index + 1]
       if temp2 != nil && temp > temp2
         array[index] = temp2
         array[index + 1] = temp
       end
+    end
   end
+  array
 end
